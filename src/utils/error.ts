00bf4ef0df3d2b3816d7868c8fa20df49184920b
dylandoamaral <do.amaral.dylan@gmail.com>
2020-04-30 16:05:08 +0200

    💻 refactor: errors format

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -2,14 +2,14 @@ import { commands } from "./command";
 
 export const error_validator_excess = (delimiter: string, template: string): string => {
     return [
-        `Error: ${delimiter} exists inside the template ${template} but you didn't give enough arguments to accept these one`,
+        `${delimiter} exists inside the template ${template} but you didn't give enough arguments to accept these one`,
         "You can change the template according to these following structure :",
         ...commands(),
     ].join("\n");
 };
 
 export const error_validator_need = (delimiter: string, template: string): string => {
-    return `Error: impossible to find ${delimiter} inside the template ${template}`;
+    return `impossible to find ${delimiter} inside the template ${template}`;
 };
 
 export const error_validator_uptodate = (): string => {
@@ -21,19 +21,19 @@ export const error_validator_pull = (): string => {
 };
 
 export const error_validator_need_multiple = (delimiter: string, template: string): string => {
-    return `Error: ${delimiter} should exist only once inside the template ${template}`;
+    return `${delimiter} should exist only once inside the template ${template}`;
 };
 
 export const error_validator_arguments = (): string => {
     return [
-        "Error: acp take between 1 and 3 arguments to work. The command should respect one of the following structure:",
+        "acp take between 1 and 3 arguments to work. The command should respect one of the following structure:",
         ...commands(),
     ].join("\n");
 };
 
 export const error_validator_map = (key: string, map: Map<string, string>, map_name: string): string => {
     return [
-        `Error: the key ${key} don't exist inside ${map_name} map. Values found inside the map:`,
+        `the key ${key} don't exist inside ${map_name} map. Values found inside the map:`,
         Array.from(map)
             .map(([key, value]) => `${key}: ${value}`)
             .map((c) => `    - ${c}`)
@@ -42,9 +42,9 @@ export const error_validator_map = (key: string, map: Map<string, string>, map_n
 };
 
 export const error_translator_undefined = (field: string): string => {
-    return `Error: can't find ${field} field inside the json`;
+    return `can't find ${field} field inside the json`;
 };
 
 export const error_builder_add_nopath = (field: string): string => {
-    return `Error: can't find the file "${field}"`;
+    return `can't find the file "${field}"`;
 };
