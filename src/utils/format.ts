c76eb395c8d9e401c98d42937378e700f101ff81
Dylan Do Amaral <do.amaral.dylan@gmail.com>
2020-05-09 16:46:30 +0200

    💻 fix: description should be optional in show function

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,23 +1,30 @@
 import { Field } from "../preset";
 import chalk from "chalk";
+import { cpus } from "os";
 
 export const space = "    - ";
 
 export const space_2 = "        • ";
 
 export const stringifyFlags = (
-    description: string,
-    ...flags: string[]
+  description: string,
+  ...flags: string[]
 ): string => {
-    return `${space}${flags
-        .map((flag) => `"${flag}"`)
-        .join(" or ")} => ${description}`;
+  return `${space}${flags
+    .map((flag) => `"${flag}"`)
+    .join(" or ")} => ${description}`;
 };
 
 export const stringifyField = ([key, field]: [string, Field]): string => {
-    const keySpacing = `${key}: `.length;
-    return [
-        `${space}${key}: ${field.value}`,
-        `${space.replace("-", " ")}${" ".repeat(keySpacing)}${field.description}`,
-    ].join("\n");
+  const keySpacing = `${key}: `.length;
+  const lines =
+    field.description === undefined
+      ? [`${space}${key}: ${field.value}`]
+      : [
+          `${space}${key}: ${field.value}`,
+          `${space.replace("-", " ")}${" ".repeat(keySpacing)}${
+            field.description
+          }`,
+        ];
+  return lines.join("\n");
 };
