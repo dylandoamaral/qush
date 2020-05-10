461c4e4244e8698223765a4bf0c4062629dca78e
Dylan Do Amaral <do.amaral.dylan@gmail.com>
2020-05-10 17:42:42 +0200

    💻 refactor: qush execution simplification

--- src/command/qush/qush.ts
+++ src/command/qush/qush.ts
@@ -8,73 +8,76 @@ import { IO, io, map as mapIO } from "fp-ts/lib/IO";
 import { execSync } from "child_process";
 import { add, commit, push } from "./builder";
 import inquirer from "inquirer";
-import { findGitRoot } from '../../utils/git';
-import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
+import { findGitRoot } from "../../utils/git";
+import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray";
 
 export interface Qush {
   args: minimist.ParsedArgs;
   preset: Preset;
 }
 
-export const toQush = ([args, preset]: [minimist.ParsedArgs, Preset]): Qush => ({
-    args,
-    preset,
+export const toQush = ([args, preset]: [
+  minimist.ParsedArgs,
+  Preset
+]): Qush => ({
+  args,
+  preset,
 });
 
 export const processQush = (qush: Qush): IO<void> =>
-    pipe(
-        io.of(execSync("git branch --show-current").toString()),
-        mapIO((branch) =>
-            pipe(
-                getFlags(qush.args, "S", "source"),
-                sourcesToAdds,
-                addsToCommands(qush)(branch),
-                (commands) => {
-                    inquirer
-                        .prompt([
-                            {
-                                type: "confirm",
-                                name: "value",
-                                message: [
-                                    "qush is going to execute these following commands for you",
-                                    commands.map((c) => `    - ${c}`).join("\n"),
-                                    "Are you agree to run them ?",
-                                ].join("\n"),
-                                default: function () {
-                                    return false;
-                                },
-                            },
-                        ])
-                        .then((answers) => {
-                            if (answers.value)
-                                commands.forEach((command) => execSync(command));
-                            else console.log("commands canceled with sucess !");
-                        });
-                }
-            )
-        )
-    );
+  pipe(
+    io.of(execSync("git branch --show-current").toString()),
+    mapIO((branch) =>
+      pipe(
+        getFlags(qush.args, "S", "source"),
+        sourcesToAdds,
+        addsToCommands(qush)(branch),
+        (commands) => {
+          inquirer
+            .prompt([
+              {
+                type: "confirm",
+                name: "value",
+                message: [
+                  "qush is going to execute these following commands for you",
+                  commands.map((c) => `    - ${c}`).join("\n"),
+                  "Are you agree to run them ?",
+                ].join("\n"),
+                default: function () {
+                  return false;
+                },
+              },
+            ])
+            .then((answers) => {
+              if (answers.value)
+                commands.forEach((command) => execSync(command));
+              else console.log("commands canceled with sucess !");
+            });
+        }
+      )
+    )
+  );
 
-export const sourcesToAdds = (sources: string[]): string[] => sources.length === 0
-    ? [add(".")]
-    : sources.map((source) => add(source));
+export const sourcesToAdds = (sources: string[]): string[] =>
+  sources.length === 0 ? [add(".")] : sources.map((source) => add(source));
 
-export const addsToCommands = (qush: Qush) => (branch: string) => (adds: string[]): string[] => [
-    ...adds,
-    commit(qush.args._ as string[], qush.preset),
-    push(branch),
+export const addsToCommands = (qush: Qush) => (branch: string) => (
+  adds: string[]
+): string[] => [
+  ...adds,
+  commit(qush.args._ as string[], qush.preset),
+  push(branch),
 ];
 
 export const qushCommand = (args: minimist.ParsedArgs): Command => ({
-    arguments: args,
-    name: "qush",
-    execute: () =>
-        pipe(
-            gitIsInstalled,
-            chain(() => rightIO(findGitRoot)),
-            chain(loadPreset),
-            chain(validate(args)),
-            map(processQush),
-            map((func) => func())
-        ),
+  arguments: args,
+  name: "qush",
+  execute: () =>
+    pipe(
+      gitIsInstalled,
+      chain(() => rightIO(findGitRoot)),
+      chain(loadPreset),
+      chain(validate(args)),
+      chain((qush) => rightIO(processQush(qush)))
+    ),
 });
