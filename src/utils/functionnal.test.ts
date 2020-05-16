f890bfadbcc56e30d7f4d98ad693b5e819167aa3
Dylan DO AMARAL <do.amaral.dylan@gmail.com>
2020-05-16 13:40:11 +0200

    💻 fix: need pull validation

--- src/utils/functionnal.test.ts
+++ src/utils/functionnal.test.ts
@@ -1,4 +1,4 @@
-import { stay, merge } from "./functionnal";
+import { stay, merge, condition } from "./functionnal";
 import { IOEither, rightIO, leftIO } from "fp-ts/lib/IOEither";
 
 describe("the stay function", () => {
@@ -13,5 +13,15 @@ describe("the merge function", () => {
         const test = (b: boolean): IOEither<Boolean, Boolean> => b ? rightIO(() => true) : leftIO(() => false);
         expect(merge(test(true))()).toEqual(true);
         expect(merge(test(false))()).toEqual(false);
+    });
+});
+
+describe("the condition function", () => {
+    it("should return the correct either", () => {
+        const left: IOEither<Boolean, Boolean> = leftIO(() => false);
+        const right: IOEither<Boolean, Boolean> = rightIO(() => true);
+
+        expect(condition(true, left, right)).toEqual(right);
+        expect(condition(false, left, right)).toEqual(left);
     });
 });
