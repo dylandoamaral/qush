f74772359ea847fc8adc3eae67e6d92e1ec56732
dylandoamaral <do.amaral.dylan@gmail.com>
2020-05-22 19:47:58 +0200

    💻 add: current version in alone command

--- src/command/alone/alone.ts
+++ src/command/alone/alone.ts
@@ -17,6 +17,8 @@ export const aloneLines = (title: string): string[] => {
         "",
         "Qush is a command to add, commit and push in only one line of code, it also allows you to keep your commit log clean by providing presets to respect.",
         "For more informations: qush --help",
+        "",
+        "current: v1.0.7"
     ];
 };
 
